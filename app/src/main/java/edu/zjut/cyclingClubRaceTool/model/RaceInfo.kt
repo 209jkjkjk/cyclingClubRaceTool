cc4abee0dfcdde414237ab0a5c49df9c9a59f902
209jkjkjk <209jkjkjk@sina.com>
2025-02-26 22:53:17 +0800

    删除无用代码

--- app/src/main/java/edu/zjut/cyclingClubRaceTool/model/RaceInfo.kt
+++ app/src/main/java/edu/zjut/cyclingClubRaceTool/model/RaceInfo.kt
@@ -3,10 +3,6 @@ package edu.zjut.cyclingClubRaceTool.model
 import java.io.Serializable
 import java.time.LocalDateTime
 
-class RaceInfo {
-    var riderList: List<Rider> = listOf()
-}
-
 data class Rider(var id: Int?, var name: String): Serializable{
     var startTime: LocalDateTime? = null
     var endTime: LocalDateTime? = null
