504197144564cea456c935a850bbfc54d8cf97c4
209jkjkjk <209jkjkjk@sina.com>
2025-02-27 21:31:56 +0800

    完善导入riders逻辑，不覆盖已有成绩的

--- app/src/main/java/edu/zjut/cyclingClubRaceTool/ui/sub/InputRiders.kt
+++ app/src/main/java/edu/zjut/cyclingClubRaceTool/ui/sub/InputRiders.kt
@@ -24,20 +24,29 @@ class InputRiders : AppCompatActivity() {
                 // 解析字符串 收集导入的数据
                 val riderStrings = text.split(Regex("\r\n|(?<!\r\n)\n(?!\r\n)|(?<!\r\n|\n)\r(?!\r\n|\n)"))  // 换行作为分隔符
                 for(riderString in riderStrings){
+                    // 过滤空行
+                    if(riderString.isEmpty()) continue
+
                     val temp = riderString.split(",")   // 逗号作为分隔符
                     val id = temp[0].toInt()
                     val name = temp[1]
+
+                    // 检查是否有重复并且有数据的，则采用原来的
+                    val findResult = AppData.riderList.filter { it.name == name && it.id == id && (it.endTime != null || it.startTime != null) }
+                    if(findResult.isNotEmpty()){
+                        tempList.addAll(findResult)
+                        continue
+                    }
+
+                    // 否则添加新的
                     tempList.add(Rider(id, name, null, null))
                 }
-                // 对于原来已经有成绩记录的数据，我们要保留
-                for(rider in AppData.riderList)
-                    if(rider.endTime != null || rider.startTime != null)
-                        tempList.add(rider)
+
                 // 列表覆盖
                 AppData.riderList = tempList
                 // 成功
                 setResult(RESULT_OK)
-                Toast.makeText(this, "导入成功！", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "导入成功！", Toast.LENGTH_SHORT).show()
                 finish()
             }catch (e:Exception){
                 Log.d("zjut", e.message?:"")
