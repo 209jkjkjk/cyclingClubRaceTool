955654ba0829b6452ddb0da41750709ff99715c3
209jkjkjk <209jkjkjk@sina.com>
2025-03-03 22:11:09 +0800

    为Rider类增加了计算成绩需要用到的属性

--- app/src/main/java/edu/zjut/cyclingClubRaceTool/model/RaceInfo.kt
+++ app/src/main/java/edu/zjut/cyclingClubRaceTool/model/RaceInfo.kt
@@ -5,13 +5,27 @@ import java.time.Duration
 import java.time.LocalDateTime
 
 data class Rider(var id: Int?, var name: String): Serializable{
+    /* 成员变量 */
     // 出发时间
     var startTime: LocalDateTime? = null
+    // 到达时间
     var finishTime: LocalDateTime? = null
     // 减秒奖励
     var timeBonus: Duration? = null
     // 注释
     var note: String? = null
+
+    val duration: Duration?
+        get() {
+        if(startTime == null || finishTime == null) return null
+        return Duration.between(startTime, finishTime)
+    }
+
+    val isDNS: Boolean
+        get() = startTime == null && finishTime != null
+
+    val isDNF: Boolean
+        get() = startTime != null && finishTime == null
 
     constructor(id: Int?, name: String, startTime: LocalDateTime?, endTime: LocalDateTime?):this(id, name){
         this.startTime = startTime
